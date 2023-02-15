f50a85cad1ca73200605a1c48e0ef5965b70e9ba
aidar61 <aidar310780@gmail.com>
2023-02-15 18:56:04 +0600

    c

--- common/src/main/java/com/ts/common/request/ApiRequest.java
+++ common/src/main/java/com/ts/common/request/ApiRequest.java
@@ -11,7 +11,6 @@ import io.restassured.specification.RequestSpecification;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
