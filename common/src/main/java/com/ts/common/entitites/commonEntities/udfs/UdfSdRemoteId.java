a11ff342b012febe34e8a4e03c12c4eb153cbbef
vpashutin <vpashutin@colvir.com>
2023-04-11 16:42:57 +0300

    Initial commit

---
+++ common/src/main/java/com/ts/common/entitites/commonEntities/udfs/UdfSdRemoteId.java
@@ -0,0 +1,22 @@
+package com.ts.common.entitites.commonEntities.udfs;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.jackson.Jacksonized;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Builder
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UdfSdRemoteId extends BaseUdfs{
+    String udfId;
+    String type;
+}
