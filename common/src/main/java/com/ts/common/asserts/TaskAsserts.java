c11bb72ed946d9f1cbd7abe3847258262fd20426
aidar61 <aidar310780@gmail.com>
2023-06-19 10:52:38 +0600

    feature/ReleaseProc-Asserts

--- common/src/main/java/com/ts/common/asserts/TaskAsserts.java
+++ common/src/main/java/com/ts/common/asserts/TaskAsserts.java
@@ -1,12 +1,36 @@
 package com.ts.common.asserts;
 
 import com.ts.common.entitites.BaseEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.testng.AssertJUnit;
+import org.testng.asserts.Assertion;
 
+import static org.testng.Assert.assertEquals;
+
+@Slf4j
 public class TaskAsserts extends EntityAssert {
     public TaskAsserts(BaseEntity entity) {
         super(entity);
     }
+
     public static TaskAsserts assertThat(BaseEntity entity) {
         return new TaskAsserts(entity);
+    }
+
+    public TaskAsserts isCorrectName(String expectedCode, String expectedPrefix) {
+        String expected = String.format("%s-%s-1.0.0", expectedCode, expectedPrefix);
+        Object actual = super.entity.receiveName();
+        AssertJUnit.assertEquals(expected, actual);
+        log.info("Task name is correct: Expected {}, Actual {}", expected, actual);
+        return this;
+    }
+
+    public TaskAsserts isCorrectShortName(String expectedPrefix) {
+        String expected = String.format("%s-1.0.0", expectedPrefix);
+        Object actual = super.entity.receiveShortName();
+        AssertJUnit.assertEquals(expected, actual);
+        log.info("Task name is correct: Expected {}, Actual {}", expected, actual);
+        return this;
     }
 }
---
+++ integration_tests/src/test/java/com/ts/integration/tests/proc_release_module/Release1Test.java
@@ -0,0 +1,71 @@
+package com.ts.integration.tests.proc_release_module;
+
+import com.ts.common.application.controllers.TrackStudioHttpStatusCodes;
+import com.ts.common.asserts.ApiAsserts;
+import com.ts.common.asserts.TaskAsserts;
+import com.ts.common.controllers.TaskResponseBody;
+import com.ts.common.controllers.release.ReleaseModuleController;
+import com.ts.common.entitites.commonEntities.Task;
+import com.ts.common.entitites.tasks.GeneralTask;
+import com.ts.common.enums.Users;
+import com.ts.common.request.ResponseBody;
+import com.ts.common.utils.InitEntities;
+import com.ts.common.utils.RandomDataUtils;
+import com.ts.common.utils.RandomUtils;
+import com.ts.integration.tests.BaseIntegrationTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.ts.common.entitites.commonEntities.Task.Constants.APNG;
+import static com.ts.common.entitites.commonEntities.Task.Constants.ONE_HUNGRED;
+import static com.ts.common.entitites.commonEntities.Udfs.UdfSd.*;
+import static com.ts.common.enums.Operations.CAT;
+import static com.ts.common.enums.Parents.OPERATION_ACC_CLIENT;
+import static com.ts.common.enums.Parents.RELEASE;
+import static com.ts.common.enums.TaskType.RELEASE_MODULE;
+import static com.ts.common.utils.InitEntities.*;
+
+public class Release1Test extends BaseIntegrationTest {
+    private ReleaseModuleController releaseModuleController;
+    private GeneralTask task;
+    private static String prefix;
+    private static String shortName;
+
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass() {
+        prefix = RandomUtils.generatePrefix();
+        shortName = RandomUtils.generateCodeShortName();
+        releaseModuleController = apiController.getReleaseModuleController();
+    }
+
+    @Test(groups = {"Release"}, description = "Создать CAT_RELEASEMODULE")
+    public void catReleaseModule() {
+        apiController.updateToken(generateAuthToken(Users.ROOT));
+        task = InitEntities.getGeneralTask(RELEASE_MODULE, CAT);
+        task.setParent(InitEntities.getParent(RELEASE));
+        task.setPriority(InitEntities.generatePriority(2));
+        task.setShortName(shortName);
+        Task udfReleaseModuleTask = RandomDataUtils.receiveRandomTaskForUdf(UDF_RELEASE_MODULE_DEPENDENCIES, task.getParent().getNumber(), OPERATION_ACC_CLIENT.tuskNumber); // receive task
+        udf = refreshUdf();
+        udf.setUdfTask(generateUdfTask(UDF_PRODUCT, APNG));
+        udf.setUdfString(generateUdfString(UDF_RELEASE_PREFIX, prefix));
+        udf.setUdfLink(generateUdfLink(UDF_RELEASE_REPOURL));
+        udf.setSecondUdfTask(generateUdfTask(UDF_RELEASE_MODULE_DEPENDENCIES, udfReleaseModuleTask));
+        udf.setSecondUdfString(generateUdfString(UDF_RELEASE_INITREV, RandomUtils.generateString()));
+        udf.setThirdUdfString(generateUdfString(UDF_RELEASE_FUNCT, RandomUtils.generateString()));
+        task.refreshUdf(udf);
+        releaseModuleController.createCatReleaseModule(task);// Сreate Task
+        ApiAsserts.assertThat(releaseModuleController.getResponse())
+                .isCorrectResponseCode(TrackStudioHttpStatusCodes.HTTP_OK)
+                .isParseableBody(TaskResponseBody.class);
+    }
+
+    @Test(groups = {"Release"}, description = "Получить форму", dependsOnMethods = "catReleaseModule")
+    public void receiveForm() {
+        apiController.updateToken(generateAuthToken(Users.ROOT));
+        releaseModuleController.receiveForm(task);
+        TaskAsserts.assertThat(task)
+                .isCorrectName(shortName, prefix)
+                .isCorrectShortName(prefix);
+    }
+}
