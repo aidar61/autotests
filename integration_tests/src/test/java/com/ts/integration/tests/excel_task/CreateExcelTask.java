e7616558b5631c844b2e8b7ed53ea56a2e055e3b
aidar61 <aidar310780@gmail.com>
2023-09-19 16:53:01 +0600

    excel

--- integration_tests/src/test/java/com/ts/integration/tests/excel_task/CreateExcelTask.java
+++ integration_tests/src/test/java/com/ts/integration/tests/excel_task/CreateExcelTask.java
@@ -44,38 +44,41 @@ public class CreateExcelTask extends BaseIntegrationTest {
         tasksFromExcel = ExcelService.readFromExcel(pathToFile);
     }
 
-    @Test(groups = {"SdQuestion", "Regression"}, description = "создание CAT_SDQUESTION")
+    @Test(groups = {"EXCEL", "Regression"}, description = "создание CAT_SDQUESTION")
     public void createTask() {
         apiController.updateToken(new AuthToken("root", "password"));
-        for (TaskFromExcelModel task1 : tasksFromExcel) {
-            controller.getParentPayload(parentNumber);
-            var module = controller.getModuleByName(task1.getUDF_SD_MODULE(), parentNumber);
-            var misService = controller.getParentUdfListValueSelector(task1.getUDF_MIS_SERVICE(), UDF_MIS_SERVICE);
-            var cdpBl = controller.getParentUdfListValueSelector(task1.getUDF_CDP_BL(), UDF_CDP_BL);
-            var taskPriority = new Status();
-            taskPriority.setId(controller.getParentFieldId(task1.getPriority(), "priorities").getId());
-            var bdku = controller.getParentUdfTask(UDF_BDKU_CONFIGURATION.udfId);
-            var workTaskAnalysis = controller.getParentUdfListValueSelector(task1.getWorkTaskAnalysis(), UDF_WORKTASK_ANALYSIS);
-            task.setName(task1.getName());
-            task.setDescription(task1.getDescription());
-            task.setParent(new Parent("818181df67e816130167eb1d99d41c73", parentNumber));
-            task.setPriority(taskPriority);
-            udf = refreshUdf();
-            udf.setUdfList(generateUdfList(UDF_CDP_BL, cdpBl.getId()));
-            udf.setUdfTask(generateUdfTask(UDF_SD_MODULE, module));
-            udf.setSecondUdfList(generateUdfList(UDF_MIS_SERVICE, misService.getId()));
-            udf.setThirdUdfList(generateUdfList(UDF_CDP_ACCEPTANCE, "ff8081813fce5b48013fce5de6b40002"));
-            udf.setFourthUdfList(generateUdfList(UDF_WORKTASK_ANALYSIS, workTaskAnalysis.getId()));
-            udf.setSecondUdfTask(generateUdfTask(UDF_PRODUCT, bdku.getTaskValue()[0]));
-            udf.setThirdUdfTask(generateUdfTask(UDF_BDKU_CONFIGURATION, new Task("8a8181df6c02a6a3016c055825e26bd7", "931164")));
-            task.refreshUdf(udf);
-            controller.createExcelTask(task);
-            var response = controller.getResponse();
-            ApiAsserts.assertThat(response)
-                    .isCorrectResponseCode(TrackStudioHttpStatusCodes.HTTP_OK)
-                    .isParseableBody(TaskResponseBody.class)
-                    .assertTask()
-                    .isEquals(task);
+        for (int i = 0; i < 1000; i++) {
+            for (TaskFromExcelModel task1 : tasksFromExcel) {
+                controller.getParentPayload(parentNumber);
+                var module = controller.getModuleByName(task1.getUDF_SD_MODULE(), parentNumber);
+                var misService = controller.getParentUdfListValueSelector(task1.getUDF_MIS_SERVICE(), UDF_MIS_SERVICE);
+                var cdpBl = controller.getParentUdfListValueSelector(task1.getUDF_CDP_BL(), UDF_CDP_BL);
+                var taskPriority = new Status();
+                taskPriority.setId(controller.getParentFieldId(task1.getPriority(), "priorities").getId());
+                var bdku = controller.getParentUdfTask(UDF_BDKU_CONFIGURATION.udfId);
+                var workTaskAnalysis = controller.getParentUdfListValueSelector(task1.getWorkTaskAnalysis(), UDF_WORKTASK_ANALYSIS);
+                task.setName(task1.getName());
+                task.setDescription(task1.getDescription());
+                task.setParent(new Parent("818181df67e816130167eb1d99d41c73", parentNumber));
+                task.setPriority(taskPriority);
+                udf = refreshUdf();
+                udf.setUdfList(generateUdfList(UDF_CDP_BL, cdpBl.getId()));
+                udf.setUdfTask(generateUdfTask(UDF_SD_MODULE, module));
+                udf.setSecondUdfList(generateUdfList(UDF_MIS_SERVICE, misService.getId()));
+                udf.setThirdUdfList(generateUdfList(UDF_CDP_ACCEPTANCE, "ff8081813fce5b48013fce5de6b40002"));
+                udf.setFourthUdfList(generateUdfList(UDF_WORKTASK_ANALYSIS, workTaskAnalysis.getId()));
+                udf.setSecondUdfTask(generateUdfTask(UDF_PRODUCT, bdku.getTaskValue()[0]));
+                udf.setThirdUdfTask(generateUdfTask(UDF_BDKU_CONFIGURATION, new Task("8a8181df6c02a6a3016c055825e26bd7", "931164")));
+                task.refreshUdf(udf);
+                controller.createExcelTask(task);
+                var response = controller.getResponse();
+                ApiAsserts.assertThat(response)
+                        .isCorrectResponseCode(TrackStudioHttpStatusCodes.HTTP_OK)
+                        .isParseableBody(TaskResponseBody.class)
+                        .assertTask()
+                        .isEquals(task);
+            }
         }
+
     }
 }
