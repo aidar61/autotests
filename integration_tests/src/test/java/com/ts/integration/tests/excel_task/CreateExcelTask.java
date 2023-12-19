dad64bf9795703100e340e1e2bc24535f3240020
aidar61 <aaskeev@colvir.com>
2023-12-19 15:26:04 +0600

    changed excel create task.

--- integration_tests/src/test/java/com/ts/integration/tests/excel_task/CreateExcelTask.java
+++ integration_tests/src/test/java/com/ts/integration/tests/excel_task/CreateExcelTask.java
@@ -1,36 +1,36 @@
 package com.ts.integration.tests.excel_task;
 
-import com.ts.common.application.controllers.AuthToken;
 import com.ts.common.application.controllers.TrackStudioHttpStatusCodes;
 import com.ts.common.asserts.ApiAsserts;
+import com.ts.common.asserts.CommonAssert;
 import com.ts.common.controllers.CreateFromExcelTaskController;
-import com.ts.common.controllers.TaskResponseBody;
-import com.ts.common.entitites.commonEntities.Parent;
-import com.ts.common.entitites.commonEntities.Status;
-import com.ts.common.entitites.commonEntities.Task;
+import com.ts.common.entitites.commonEntities.*;
 import com.ts.common.entitites.tasks.GeneralTask;
 import com.ts.common.enums.Operations;
 import com.ts.common.enums.TaskType;
 import com.ts.common.services.ExcelService;
 import com.ts.common.services.models.TaskFromExcelModel;
 import com.ts.common.utils.InitEntities;
+import com.ts.common.utils.StringUtils;
 import com.ts.integration.tests.BaseIntegrationTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
+import static com.ts.common.entitites.commonEntities.List.Constants.*;
 import static com.ts.common.entitites.commonEntities.Udfs.UdfSd.*;
 import static com.ts.common.utils.InitEntities.*;
 
 public class CreateExcelTask extends BaseIntegrationTest {
+    private static final String pathToFile = "src/test/resources/tasks.xlsx";
+    private static final String parentNumber = "849976";
     public CreateFromExcelTaskController controller;
     private GeneralTask task;
     private List<TaskFromExcelModel> tasksFromExcel;
-    private static final String pathToFile = "src/test/resources/tasks.xlsx";
-    private static final String parentNumber = "849976";
-
+    private List<UserRole> userRoles;
 
     @BeforeClass(alwaysRun = true)
     public void beforeClass() throws IOException {
@@ -43,41 +43,82 @@ public class CreateExcelTask extends BaseIntegrationTest {
     @Test(groups = {"EXCEL"}, description = "создание CAT_SDQUESTION")
     public void createTask() {
         int j = 0;
-        apiController.updateToken(new AuthToken("root", "password"));
-        for (int i = 0; i < 1000; i++) {
-            for (TaskFromExcelModel task1 : tasksFromExcel) {
-                controller.getParentPayload(parentNumber);
-                var module = controller.getModuleByName(task1.getUDF_SD_MODULE(), parentNumber);
-                var misService = controller.getParentUdfListValueSelector(task1.getUDF_MIS_SERVICE(), UDF_MIS_SERVICE);
-                var cdpBl = controller.getParentUdfListValueSelector(task1.getUDF_CDP_BL(), UDF_CDP_BL);
-                var taskPriority = new Status();
-                taskPriority.setId(controller.getParentFieldId(task1.getPriority(), "priorities").getId());
-                var bdku = controller.getParentUdfTask(UDF_BDKU_CONFIGURATION.udfId);
-                var workTaskAnalysis = controller.getParentUdfListValueSelector(task1.getWorkTaskAnalysis(), UDF_WORKTASK_ANALYSIS);
-                task.setName(task1.getName());
-                task.setDescription(task1.getDescription());
-                task.setParent(new Parent("818181df67e816130167eb1d99d41c73", parentNumber));
-                task.setPriority(taskPriority);
-                udf = refreshUdf();
-                udf.setUdfList(generateUdfList(UDF_CDP_BL, cdpBl.getId()));
-                udf.setUdfTask(generateUdfTask(UDF_SD_MODULE, module));
-                udf.setSecondUdfList(generateUdfList(UDF_MIS_SERVICE, misService.getId()));
-                udf.setThirdUdfList(generateUdfList(UDF_CDP_ACCEPTANCE, "ff8081813fce5b48013fce5de6b40002"));
-                udf.setFourthUdfList(generateUdfList(UDF_WORKTASK_ANALYSIS, workTaskAnalysis.getId()));
-                udf.setSecondUdfTask(generateUdfTask(UDF_PRODUCT, bdku.getTaskValue()[0]));
-                udf.setThirdUdfTask(generateUdfTask(UDF_BDKU_CONFIGURATION, new Task("8a8181df6c02a6a3016c055825e26bd7", "931164")));
-                task.refreshUdf(udf);
-                controller.createExcelTask(task);
-                var response = controller.getResponse();
-                ApiAsserts.assertThat(response)
-                        .isCorrectResponseCode(TrackStudioHttpStatusCodes.HTTP_OK)
-                        .isParseableBody(TaskResponseBody.class)
-                        .assertTask()
-                        .isEquals(task);
-               System.out.printf("======================================TASK NUMBER IS %s=======================================", j++);
-               if (j > 3000) break;
-            }
-        }
+        for (TaskFromExcelModel task1 : tasksFromExcel) {
+            controller.getParentPayload(parentNumber);
+            getParticipants(parentNumber);
+            var creator = getByName(task1.getCreator());
+            var handler = getByName(task1.getHandler());
+            var watcher = getByName(task1.getUDF_WATCHER());
 
+            apiController.updateToken(generateAuthToken(creator));
+            var cdpBl = controller
+                    .getParentUdfListValueSelector(task1.getUDF_CDP_BL(), UDF_CDP_BL);
+            var module = controller
+                    .getModuleByName(task1.getUDF_SD_MODULE(), parentNumber);
+            var misService = controller
+                    .getParentUdfListValueSelector(task1.getUDF_MIS_SERVICE(), UDF_MIS_SERVICE);
+            var taskPriority = Status
+                    .builder()
+                    .id(controller.getParentFieldId(task1.getPriority(), "priorities").getId())
+                    .build();
+            var workTaskAnalysis = task1.getUDF_WORKTASK_ANALYSIS().equals("Не требуется") ?
+                    UDF_WORKTASK_ANALYSIS_NO : UDF_WORKTASK_ANALYSIS_YES;
+            var bdkuConfiguration = Task
+                    .builder()
+                    .number(StringUtils.getTaskNumber(task1.getUDF_BDKU_CONFIGURATION()))
+                    .build();
+            var linkedTask = Task
+                    .builder()
+                    .number(StringUtils.getTaskNumber(task1.getUDF_SD_LINKEDREQUEST()))
+                    .build();
+            var udfProduct = controller.getUdfProduct().getTaskValueSelector()[0];
+
+            task.refreshTask();
+            if (handler != null)
+                task.setHandlerUser(handler);
+            task.setName(task1.getName());
+            task.setDescription(task1.getDescription());
+            task.setParent(new Parent("818181df67e816130167eb1d99d41c73", parentNumber));
+            task.setPriority(taskPriority);
+            udf = refreshUdf();
+            udf.setUdfList(generateUdfList(UDF_CDP_BL, cdpBl.getId()));
+            udf.setUdfTask(generateUdfTask(UDF_SD_MODULE, module));
+            udf.setSecondUdfList(generateUdfList(UDF_MIS_SERVICE, misService.getId()));
+            udf.setFourthUdfList(generateUdfList(UDF_WORKTASK_ANALYSIS, workTaskAnalysis.getId()));
+            udf.setSecondUdfTask(generateUdfTask(UDF_SD_LINKEDREQUEST, linkedTask));
+            udf.setThirdUdfTask(generateUdfTask(UDF_BDKU_CONFIGURATION, bdkuConfiguration));
+            udf.setFourthUdfTask(generateUdfTask(UDF_PRODUCT, udfProduct));
+            udf.setFifthUdfList(generateUdfList(UDF_CDP_ACCEPTANCE, UDF_CDP_ACCEPTANCE_NO));
+            if (watcher != null) {
+                udf.setUdfUser(generateUdfUser(UDF_WATCHER, watcher));
+            }
+            task.refreshUdf(udf);
+            controller.createExcelTask(task);
+            var response = controller.getResponse();
+            ApiAsserts.assertThat(response)
+                    .isCorrectResponseCode(TrackStudioHttpStatusCodes.HTTP_OK);
+            CommonAssert
+                    .assertThat(response)
+                    .isCorrectUdfList(UDF_CDP_BL, cdpBl.getId())
+//                    .isCorrectUdfList(UDF_MIS_SERVICE, misService.getId())
+                    .isCorrectUdfList(UDF_WORKTASK_ANALYSIS, workTaskAnalysis.getId())
+                    .isCorrectUdfList(UDF_CDP_ACCEPTANCE, UDF_CDP_ACCEPTANCE_NO.getId())
+                    .isCorrectUdfTask(UDF_SD_MODULE, module.getNumber())
+                    .isCorrectUdfTask(UDF_BDKU_CONFIGURATION, bdkuConfiguration.getNumber())
+                    .isCorrectUdfTask(UDF_PRODUCT, udfProduct.getNumber());
+
+            System.out.printf("======================================TASK NUMBER IS %s=======================================", j++);
+        }
+    }
+
+    private void getParticipants(String parentNumber) {
+        userRoles = userController.receiveUserByTask(parentNumber);
+    }
+
+    private User getByName(String name) {
+        return Objects.requireNonNullElse(userRoles.stream()
+                .filter(f -> f.getForUser().getName().equals(name) &&
+                        f.getForUser().getActive())
+                .findFirst().orElse(null), new UserRole()).getForUser();
     }
 }
