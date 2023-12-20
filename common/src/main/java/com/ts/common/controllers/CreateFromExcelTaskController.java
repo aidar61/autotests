6804426204a3bce6cc345cb4d34b280a6a9b6008
aidar61 <aidar310780@gmail.com>
2023-12-20 19:25:16 +0600

    feature/Mass tasks

--- common/src/main/java/com/ts/common/controllers/CreateFromExcelTaskController.java
+++ common/src/main/java/com/ts/common/controllers/CreateFromExcelTaskController.java
@@ -6,8 +6,10 @@ import com.ts.common.entitites.commonEntities.Task;
 import com.ts.common.entitites.commonEntities.Udfs;
 import com.ts.common.entitites.commonEntities.udf.UdfTask;
 import com.ts.common.entitites.tasks.GeneralTask;
+import com.ts.common.enums.Operations;
 import com.ts.common.enums.TaskType;
 import com.ts.common.services.models.ListValueSelector;
+import com.ts.common.utils.InitEntities;
 import com.ts.common.utils.JsonUtils;
 import io.qameta.allure.Step;
 import io.restassured.path.json.JsonPath;
@@ -38,7 +40,15 @@ public class CreateFromExcelTaskController extends BaseController {
     }
 
     public Response addTag(String taskNumber, Tag data) {
-        return this.response = super.post(getEndpoint(REST, TASK, TAG, taskNumber),data);
+        return this.response = super.post(getEndpoint(REST, TASK, TAG, taskNumber), data);
+    }
+
+    public Response toClientTest(GeneralTask task) {
+        task.setOperation(InitEntities.generateOperationID(TaskType.WORK_TASK, Operations.TOCLIENTTEST));
+        task.setDescription("");
+        TaskRequestBody taskRequestBody = new TaskRequestBody(task);
+        return this.response = super.performOperationWithQueryParam(task
+                , taskRequestBody.keepFields(DEFAULT_FIELDS_USER));
     }
 
     public String getParentCDP_BL(String searchTerm) {
--- integration_tests/src/test/java/com/ts/integration/tests/excel_task/CreateExcelTask.java
+++ integration_tests/src/test/java/com/ts/integration/tests/excel_task/CreateExcelTask.java
@@ -7,6 +7,7 @@ import com.ts.common.entitites.commonEntities.*;
 import com.ts.common.entitites.tasks.GeneralTask;
 import com.ts.common.enums.Operations;
 import com.ts.common.enums.TaskType;
+import com.ts.common.enums.Users;
 import com.ts.common.services.ExcelService;
 import com.ts.common.services.models.TaskFromExcelModel;
 import com.ts.common.utils.InitEntities;
@@ -20,12 +21,13 @@ import java.util.List;
 import java.util.Objects;
 
 import static com.ts.common.entitites.commonEntities.List.Constants.*;
+import static com.ts.common.entitites.commonEntities.Task.Constants.TENGE_BANK;
 import static com.ts.common.entitites.commonEntities.Udfs.UdfSd.*;
 import static com.ts.common.utils.InitEntities.*;
 
 public class CreateExcelTask extends BaseIntegrationTest {
     private static final String pathToFile = "src/test/resources/tasks.xlsx";
-    private static final String parentNumber = "1506042";
+    private static final String parentNumber = "1506459";
     public CreateFromExcelTaskController controller;
     private GeneralTask task;
     private List<TaskFromExcelModel> tasksFromExcel;
@@ -36,6 +38,7 @@ public class CreateExcelTask extends BaseIntegrationTest {
         controller = apiController.getCreateFromExcelTaskController();
         userController = apiController.getUserController();
         task = InitEntities.getGeneralTask(TaskType.CAT_CFGTASK, Operations.CAT);
+        // получаем задачи из excel таблицы
         tasksFromExcel = ExcelService.readFromExcel(pathToFile);
     }
 
@@ -44,7 +47,10 @@ public class CreateExcelTask extends BaseIntegrationTest {
         int j = 0;
         for (TaskFromExcelModel task1 : tasksFromExcel) {
             controller.getParentPayload(parentNumber);
+            //получаем участников этой задачи по ее номеру
             getParticipants(parentNumber);
+            System.err.println(task1);
+            System.err.println(task1.getHandler());
             var creator = getByName(task1.getCreator());
             var handler = getByName(task1.getHandler());
             var watcher = getByName(task1.getUDF_WATCHER());
@@ -74,35 +80,47 @@ public class CreateExcelTask extends BaseIntegrationTest {
 
             task.setName(task1.getName());
             task.setDescription(task1.getDescription());
-            task.setParent(new Parent("818181678c8669a0018c86975b3c000b", parentNumber));
+            task.setParent(new Parent("818181678c8669a0018c875752c1451b", parentNumber));
             task.setPriority(taskPriority);
             udf = refreshUdf();
             udf.setUdfList(generateUdfList(UDF_CDP_BL, cdpBl.getId()));
-            udf.setUdfTask(generateUdfTask(UDF_SD_MODULE, module));
             udf.setSecondUdfList(generateUdfList(UDF_MIS_SERVICE, misService.getId()));
-            udf.setFourthUdfList(generateUdfList(UDF_WORKTASK_ANALYSIS, workTaskAnalysis.getId()));
+            udf.setThirdUdfList(generateUdfList(UDF_WORKTASK_ANALYSIS, workTaskAnalysis.getId()));
+            udf.setUdfTask(generateUdfTask(UDF_SD_MODULE, module));
             udf.setSecondUdfTask(generateUdfTask(UDF_SD_LINKEDREQUEST, linkedTask));
-            udf.setThirdUdfTask(generateUdfTask(UDF_BDKU_CONFIGURATION, bdkuConfiguration));
-            udf.setFourthUdfTask(generateUdfTask(UDF_PRODUCT, udfProduct));
+            udf.setThirdUdfTask(generateUdfTask(UDF_BDKU_CONFIGURATION, TENGE_BANK));
+            udf.setFourthUdfTask(generateUdfTask(UDF_PRODUCT, bdkuConfiguration));
             udf.setFifthUdfList(generateUdfList(UDF_CDP_ACCEPTANCE, UDF_CDP_ACCEPTANCE_NO));
-            if (handler != null) {
-                task.setHandlerUser(handler);
-                udf.setSecondUdfUser(generateUdfUser(STDT_HANDLER, handler));
-            }
-            if (watcher != null) {
-                udf.setUdfUser(generateUdfUser(UDF_WATCHER, watcher));
-            }
+//            if (handler != null) {
+//                task.setHandlerUser(handler);
+//                udf.setSecondUdfUser(generateUdfUser(STDT_HANDLER, handler));
+//            }
+//            if (watcher != null) {
+//                udf.setUdfUser(generateUdfUser(UDF_WATCHER, watcher));
+//            }
             task.refreshUdf(udf);
             controller.createExcelTask(task);
             var response = controller.getResponse();
             ApiAsserts.assertThat(response)
                     .isCorrectResponseCode(TrackStudioHttpStatusCodes.HTTP_OK);
-
+//
+            // Добавляем тэг
             var addTagResponse = controller.addTag(task.getNumber(), Tag
                     .builder()
                     .tag(task1.getTag())
                     .build());
             ApiAsserts.assertThat(addTagResponse)
+                    .isCorrectResponseCode(TrackStudioHttpStatusCodes.HTTP_OK);
+
+            // Выполняем операцию Передать на проверку клиенту указывая ответственного handler
+            udf = refreshUdf();
+            udf.setUdfUser(generateUdfUser(STDT_HANDLER, handler));
+            task.setHandlerUser(handler);
+            task.setDescription("");
+            task.refreshUdf(udf);
+            var responseToClientTest = controller.toClientTest(task);
+
+            ApiAsserts.assertThat(responseToClientTest)
                     .isCorrectResponseCode(TrackStudioHttpStatusCodes.HTTP_OK);
 //            CommonAssert
 //                    .assertThat(response)
@@ -114,7 +132,7 @@ public class CreateExcelTask extends BaseIntegrationTest {
 //                    .isCorrectUdfTask(UDF_BDKU_CONFIGURATION, bdkuConfiguration.getNumber())
 //                    .isCorrectUdfTask(UDF_PRODUCT, udfProduct.getNumber());
 
-            System.out.printf("======================================TASK NUMBER IS %s=======================================", j++);
+            System.err.printf("======================================TASK NUMBER IS %s=======================================", j++);
         }
     }
 
@@ -124,7 +142,7 @@ public class CreateExcelTask extends BaseIntegrationTest {
 
     private User getByName(String name) {
         return Objects.requireNonNullElse(userRoles.stream()
-                .filter(f -> f.getForUser().getName().equals(name) &&
+                .filter(f -> f.getForUser().getName().contains(name) &&
                         f.getForUser().getActive())
                 .findFirst().orElse(null), new UserRole()).getForUser();
     }
