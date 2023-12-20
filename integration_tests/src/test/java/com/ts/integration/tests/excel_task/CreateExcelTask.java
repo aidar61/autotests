1d38a914dd289f951a76c35285652cea0056b3a0
aidar61 <aidar310780@gmail.com>
2023-12-20 14:26:23 +0600

    feature/Mass tasks

--- integration_tests/src/test/java/com/ts/integration/tests/excel_task/CreateExcelTask.java
+++ integration_tests/src/test/java/com/ts/integration/tests/excel_task/CreateExcelTask.java
@@ -25,7 +25,7 @@ import static com.ts.common.utils.InitEntities.*;
 
 public class CreateExcelTask extends BaseIntegrationTest {
     private static final String pathToFile = "src/test/resources/tasks.xlsx";
-    private static final String parentNumber = "849976";
+    private static final String parentNumber = "1405674";
     public CreateFromExcelTaskController controller;
     private GeneralTask task;
     private List<TaskFromExcelModel> tasksFromExcel;
@@ -74,7 +74,7 @@ public class CreateExcelTask extends BaseIntegrationTest {
 
             task.setName(task1.getName());
             task.setDescription(task1.getDescription());
-            task.setParent(new Parent("818181df67e816130167eb1d99d41c73", parentNumber));
+            task.setParent(new Parent("8181816a8c3f19fa018c8631b305144d", parentNumber));
             task.setPriority(taskPriority);
             udf = refreshUdf();
             udf.setUdfList(generateUdfList(UDF_CDP_BL, cdpBl.getId()));
