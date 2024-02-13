b79d9f56b9f726b58b7f18b7de482f5b39ba5653
aidar61 <aidar310780@gmail.com>
2024-02-13 19:11:56 +0600

    feature/performance tests

--- common/src/main/java/com/ts/common/controllers/performance/PerformanceController.java
+++ common/src/main/java/com/ts/common/controllers/performance/PerformanceController.java
@@ -4,14 +4,18 @@ import com.ts.common.application.controllers.AuthToken;
 import com.ts.common.config.AppConfigProvider;
 import com.ts.common.controllers.BaseController;
 import com.ts.common.entitites.commonEntities.User;
+import com.ts.common.entitites.tasks.GeneralTask;
 import com.ts.common.enums.Performance;
+import com.ts.common.enums.TaskType;
 import com.ts.common.enums.Users;
 import com.ts.common.utils.InitEntities;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
 import java.util.Map;
 
 import static com.ts.common.application.controllers.TrackStudioEndPoints.*;
+import static com.ts.common.enums.Operations.CANCEL;
 import static com.ts.common.enums.Performance.*;
 
 public class PerformanceController extends BaseController {
@@ -19,6 +23,7 @@ public class PerformanceController extends BaseController {
         super(url, authToken);
     }
 
+    @Step("Запрос портлета \"Оперативный контроль доработок\"")
     public void getOperationControlOf(Performance performanceColumn) {
         super.get(getEndpoint(REST, PORTLET, SLA_FEATURE_CONTROL, GET_SLA_FEATURE_CONTROL
                 , formatParameters(Map.of(
@@ -27,10 +32,19 @@ public class PerformanceController extends BaseController {
                 ))));
     }
 
+    @Step("Запрос портлета \"Очередь задач сотрудника\"")
     public void getPortletOfEmployeeTasksQueue(User user) {
         super.get(getEndpoint(REST, PORTLET, PERSONAL_QUEUE, user.getLogin(), TASKS
                 , formatParameters(Map.of(
                         "columns", COLUMNS_PORTLET_EMPLOYEE_QUEUE.value
                 ))));
     }
+
+    public void cancelTask(GeneralTask task) {
+        task.refreshUdf();
+        task.refreshTask();
+        task.setTaskType(TaskType.WORK_TASK);
+        super.performCommonOperation(task, CANCEL);
+    }
+
 }
