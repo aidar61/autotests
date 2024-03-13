7d8768b7320f15f205a5f4bb02763961682df2f0
aidar61 <aidar310780@gmail.com>
2024-03-13 12:14:18 +0600

    fix/tests

---
+++ integration_tests/src/test/java/com/ts/integration/tests/performance_tests/GeneratorTests.java
@@ -0,0 +1,10 @@
+package com.ts.integration.tests.performance_tests;
+
+import com.ts.common.controllers.UserController;
+import com.ts.common.entitites.tasks.GeneralTask;
+import com.ts.integration.tests.BaseIntegrationTest;
+
+public class GeneratorTests extends BaseIntegrationTest {
+    UserController userController;
+    GeneralTask task;
+}
