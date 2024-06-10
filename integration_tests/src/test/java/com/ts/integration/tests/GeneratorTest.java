8ede2ea484a4cb940a041ae26b2da428f41bc569
aidar61 <aidar310780@gmail.com>
2024-06-10 14:15:47 +0600

    fix/resolve conflicts

---
+++ integration_tests/src/test/java/com/ts/integration/tests/GeneratorTest.java
@@ -0,0 +1,10 @@
+package com.ts.integration.tests;
+
+import org.testng.annotations.Test;
+
+public class GeneratorTest extends BaseIntegrationTest {
+    @Test(groups = {"GENERATOR"}, description = "Generating tasks and user for precondition")
+    public void generator() {
+
+    }
+}
